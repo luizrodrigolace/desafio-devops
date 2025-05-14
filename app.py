cb6be7150f1877bf1a7d8c7259238ac3f007bbd8
Luiz Rodrigo Lace <luizrodrigolace@gmail.com>
2025-05-13 21:47:05 -0300

    style: formata app.py com black

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME","World")
+    name = os.environ.get("NAME", "World")
     return f"Hello, {name}!"
 
 
